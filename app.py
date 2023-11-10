4675504e205123107fe310ec27bf3db1ca017362
jeevjacobgeorge <jeev@J4Hp>
2023-11-10 09:12:22 +0530

    Update Task feature added

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
-#python3
-#from app import db
-#db.create_all()
+
 class Todo(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     content = db.Column(db.String(200),nullable = False)
@@ -41,7 +39,6 @@ def index():
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
-
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
@@ -49,9 +46,20 @@ def delete(id):
     except:
         return 'There was a problem deleting that task'
 
-# @app.route('/update/<int:id>')
-# def update(id):
-#     task
+@app.route('/update/<int:id>',methods=['GET','POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content= request.form.get('content')
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was a issue updating your task"
+    else:
+        return render_template('update.html',task=task)
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
